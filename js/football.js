113800ce1048e643531ea8f7076ebd284e7c0882
hai1222 <1283293845@qq.com>
2015-07-20 17:02:37 +0800

    222

---
+++ js/football.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$.getJSON('http://101.251.229.158:8686?callback=?',function(data){
+		console.log(data);
+		var len = data.length;
+		var addhtml = '';
+		for(var i = 0;i < len;i++){
+			addhtml += '<tr>'+
+				'<td>' + data[i].zhu + '</td>' + 
+				'<td>' + data[i].ke + '</td>' + 
+				'<td>' + data[i].liansai + '</td>' + 
+				'<td>' + data[i].quan + '</td>' + 
+				'<td>' + data[i].ban + '</td>' + 
+				'<td>' + data[i].time + '</td>' + 
+				'</tr>';
+		}
+		$('.table tbody').html("");
+		$('.table tbody').html(addhtml);
+	})
+})
