61303cdbd8a9dba685e21621bad6dffe45466181
hai1222 <1283293845@qq.com>
2019-04-02 06:31:52 +0800

    big big house

--- js/fibseq.js
+++ js/fibseq.js
@@ -10,3 +10,20 @@ var fibObject = {
 	"89":{"bull":55,"bear":34,"prev":55,"next":144},
 	"144":{"bull":89,"bear":55,"prev":89,"next":233}
 };
+
+var models = {
+    '2019-04-01': {
+        'group1-1': 0,
+        'group1-2': 0,
+        'group1-3': 0,//s近10-5+10
+        'group1-4': 0,//s近10-10+10
+        'group1-5': -550,//s实10-5+10
+        'group1-6': 0,
+        'group2-1': 0,
+        'group2-2': 0,
+        'group2-3': 0,//m近10-5+10
+        'group2-4': 0,//m近10-10+10
+        'group2-5': 0,
+        'group2-6': 0
+    }
+};
