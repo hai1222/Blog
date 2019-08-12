f1bc8a31eca715e3562a21ea5570e64cee0e1cf3
v_maozhiqiang <v_maozhiqiang@baidu.com>
2019-08-12 19:55:16 +0800

    big big house

---
+++ js/house.js
@@ -0,0 +1,13 @@
+var fibObject = {
+	"3":{"bull":2,"bear":1,"prev":null,"next":5},
+	"5":{"bull":3,"bear":2,"prev":3,"next":8},
+	"8":{"bull":5,"bear":3,"prev":5,"next":13},
+	"13":{"bull":8,"bear":5,"prev":8,"next":21},
+	"21":{"bull":13,"bear":8,"prev":13,"next":34},
+	"34":{"bull":21,"bear":13,"prev":21,"next":55},
+	"55":{"bull":34,"bear":21,"prev":34,"next":89},
+	"55":{"bull":34,"bear":21,"prev":34,"next":89},
+	"89":{"bull":55,"bear":34,"prev":55,"next":144},
+	"144":{"bull":89,"bear":55,"prev":89,"next":233},
+    "233":{"bull":144,"bear":89,"prev":144,"next":377}
+};
